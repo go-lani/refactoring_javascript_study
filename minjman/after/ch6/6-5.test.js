a7761d2affd078b1bf135a859108d79b9c7e3582
minjman2659 <minjman2659@gmail.com>
2022-08-12 21:18:15 +0900

    [김민재] 6-5-2 Test 코드 작성

--- minjman/after/ch6/6-5.test.js
+++ minjman/after/ch6/6-5.test.js
@@ -1,5 +1,5 @@
 import { circumference } from './6-5-1';
-// import { Book } from './6-5-2';
+import Book from './6-5-2';
 import { inNewEngland } from './6-5-3';
 
 describe('6-5 Test', () => {
@@ -9,6 +9,29 @@ describe('6-5 Test', () => {
     });
     it('circumference should return number', () => {
       expect(circumference(3)).toBe(18.84955592153876);
+    });
+  });
+
+  describe('6-5-2 class Book', () => {
+    let book = null;
+    let customer1 = null;
+    let customer2 = null;
+    beforeEach(() => {
+      book = new Book();
+      customer1 = { id: 1 };
+      customer2 = { id: 2 };
+    });
+    it('isPriority customer should be returned first when isPriority is true', () => {
+      book.addReservation(customer1);
+      book.addReservation(customer2, true);
+      expect(book.firstCustomer).toEqual(customer2);
+    });
+    it('should be true when customer is exist', () => {
+      book.addReservation(customer2);
+      expect(book.hasReservation(customer2)).toBe(true);
+    });
+    it('should be false when customer is not exist', () => {
+      expect(book.hasReservation(customer2)).toBe(false);
     });
   });
 
