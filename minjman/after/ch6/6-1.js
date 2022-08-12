e90cb0ec4dd7bdfb4b795b8ec30946ad8ac5230d
minjman2659 <minjman2659@gmail.com>
2022-08-12 17:17:09 +0900

    [김민재] ch6/6-1.js 리팩토링 및 yarn 설정

--- minjman/after/ch6/6-1.js
+++ minjman/after/ch6/6-1.js
@@ -1,1 +1,25 @@
-console.log("6-1.js");
+import {
+  printBanner,
+  calculateOutstanding,
+  recordDueDate,
+  printDetails,
+} from './utils/6-1.js';
+
+export function printOwing(invoice) {
+  printBanner();
+
+  // calculate outstanding
+  const { outstanding } = calculateOutstanding(invoice);
+
+  // record due date
+  recordDueDate(invoice);
+
+  //print details
+  printDetails(invoice, outstanding);
+}
+
+const invoice = {
+  orders: [{ amount: 2 }, { amount: 5 }],
+  customer: '엘리',
+};
+printOwing(invoice);
