e4e2f711c037ffbdfe09e45c0059a3184883b50e
publee <publizm@gmail.com>
2022-08-16 02:14:34 +0900

    [이철환] 6-2 리팩토링 코드 검증 기능 추가

--- publee/after/ch6/6-2.js
+++ publee/after/ch6/6-2.js
@@ -1,8 +1,20 @@
-// 예제 1
-export const rating = (driver) => Number(driver.numberOfLateDeliveries > 5) + 1;
+import isObject from "lodash/isObject";
+import isEmpty from "lodash/isEmpty";
 
-// 예제 2
-export const reportLines = (customer) => [
-  ["name", customer.name],
-  ["location", customer.location],
-];
+export const rating = (driver) => {
+  if (!isObject(driver) || typeof driver.numberOfLateDeliveries !== "number") {
+    throw new Error("잘못된 값이 입력됬습니다");
+  }
+  return driver.numberOfLateDeliveries > 5 ? 2 : 1;
+};
+
+export const reportLines = (customer) => {
+  if (!isObject(customer) || isEmpty(customer)) {
+    throw new Error("잘못된 값이 입력됬습니다");
+  }
+
+  return [
+    ["name", customer.name],
+    ["location", customer.location],
+  ];
+};
