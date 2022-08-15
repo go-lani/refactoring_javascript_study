7d0fba98b6c05d92696a7b29b40c67becd4a23f2
publee <publizm@gmail.com>
2022-08-16 01:28:29 +0900

    [이철환] 6-1 테스트 코드 검증 기능 추가

--- publee/after/ch6/6-1.test.js
+++ publee/after/ch6/6-1.test.js
@@ -9,7 +9,25 @@ describe("6-1 Test", () => {
     };
   });
 
-  it("Success - invoice is not Empty", () => {
+  describe("Error Case - Invalid Input Value", () => {
+    it("When a null value is entered", () => {
+      expect(() => printOwing(null)).toThrow("잘못된 값이 입력됬습니다");
+    });
+    it("When a Empty value is entered", () => {
+      expect(() => printOwing()).toThrow("잘못된 값이 입력됬습니다");
+    });
+    it("When a Array is entered", () => {
+      expect(() => printOwing([])).toThrow("잘못된 값이 입력됬습니다");
+    });
+    it("When a Number is entered", () => {
+      expect(() => printOwing(0)).toThrow("잘못된 값이 입력됬습니다");
+    });
+    it("When a String is entered", () => {
+      expect(() => printOwing("Hello")).toThrow("잘못된 값이 입력됬습니다");
+    });
+  });
+
+  it("Success Case - invoice is not Empty", () => {
     expect(printOwing(invoice)).toBeUndefined();
   });
 });
