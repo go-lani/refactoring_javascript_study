2717dda490d19f28528696ffa22a28016d6ed7ab
publee <publizm@gmail.com>
2022-08-16 01:01:45 +0900

    [이철환] 6-1 테스트 코드 작성

--- publee/after/ch6/6-1.test.js
+++ publee/after/ch6/6-1.test.js
@@ -1,1 +1,15 @@
-console.log("6-1.js");
+import { printOwing } from "./6-1";
+
+describe("6-1 Test", () => {
+  let invoice;
+  beforeEach(() => {
+    invoice = {
+      orders: [{ amount: 2 }, { amount: 5 }],
+      customer: "엘리",
+    };
+  });
+
+  it("Success - invoice is not Empty", () => {
+    expect(printOwing(invoice)).toBeUndefined();
+  });
+});
