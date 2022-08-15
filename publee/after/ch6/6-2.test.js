5dc6e3ea3de735f2199c402422132ec240f66167
publee <publizm@gmail.com>
2022-08-16 02:14:26 +0900

    [이철환] 6-2 테스트코드 검증 기능 추가

--- publee/after/ch6/6-2.test.js
+++ publee/after/ch6/6-2.test.js
@@ -10,21 +10,57 @@ describe("6-2 Test", () => {
   });
 
   describe("6-2 rating test", () => {
-    it("number of late deliveries more than 5", () => {
-      expect(rating({ numberOfLateDeliveries: 6 })).toBe(2);
+    describe("Success - rating function return 1 or 2", () => {
+      it("number of late deliveries more than 5", () => {
+        expect(rating({ numberOfLateDeliveries: 6 })).toBe(2);
+      });
+
+      it("number of late deliveries less than 5", () => {
+        expect(rating({ numberOfLateDeliveries: 4 })).toBe(1);
+      });
+
+      it("number of late deliveries is 5", () => {
+        expect(rating({ numberOfLateDeliveries: 5 })).toBe(1);
+      });
     });
 
-    it("number of late deliveries less than 5", () => {
-      expect(rating({ numberOfLateDeliveries: 4 })).toBe(1);
-    });
+    describe("Validtate rating function Invalid Value", () => {
+      it("When a Array is entered", () => {
+        expect(() => rating([])).toThrow("잘못된 값이 입력됬습니다");
+      });
 
-    it("number of late deliveries is 5", () => {
-      expect(rating({ numberOfLateDeliveries: 5 })).toBe(1);
+      it("When a Empty Object is entered", () => {
+        expect(() => rating({})).toThrow("잘못된 값이 입력됬습니다");
+      });
+
+      it("When a String is entered", () => {
+        expect(() => rating({ numberOfLateDeliveries: "" })).toThrow(
+          "잘못된 값이 입력됬습니다"
+        );
+      });
     });
   });
 
   describe("6-2 reportLines test", () => {
-    it("reportLinnes function is return array", () => {
+    describe("Validtate reportLines function Invalid Value", () => {
+      it("When a Array is entered", () => {
+        expect(() => reportLines([])).toThrow("잘못된 값이 입력됬습니다");
+      });
+
+      it("When a Empty Object is entered", () => {
+        expect(() => reportLines({})).toThrow("잘못된 값이 입력됬습니다");
+      });
+
+      it("When a String is entered", () => {
+        expect(() => reportLines("Hello")).toThrow("잘못된 값이 입력됬습니다");
+      });
+
+      it("When a String is entered", () => {
+        expect(() => reportLines(123)).toThrow("잘못된 값이 입력됬습니다");
+      });
+    });
+
+    it("Success - reportLines function return array", () => {
       expect(reportLines(customer)).toEqual([
         ["name", "publee"],
         ["location", "seoul"],
