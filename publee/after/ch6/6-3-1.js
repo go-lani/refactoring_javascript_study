8781f2e648314a49ab60d86cb4fb10a354cb84ce
publee <publizm@gmail.com>
2022-08-16 16:59:04 +0900

    [이철환] 6-3-1 리팩토링 작성

---
+++ publee/after/ch6/6-3-1.js
@@ -0,0 +1,8 @@
+export function price(order) {
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const deliveryCharge = Math.min(basePrice * 0.1, 100);
+
+  return basePrice - quantityDiscount + deliveryCharge;
+}
