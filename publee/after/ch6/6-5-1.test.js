461c9ff9ca135044d6449e5748a9d9bb30049450
publee <publizm@gmail.com>
2022-08-16 17:44:11 +0900

    [이철환] 6-5-1 통과 테스트코드 작성

---
+++ publee/after/ch6/6-5-1.test.js
@@ -0,0 +1,8 @@
+import { circum } from "./6-5-1.js";
+
+describe("6-5-1 Test", () => {
+  const radius = 3;
+  it("radius 값의 반지름을 가진 원의 둘레는 2 * Math.PI * radius 이다.", () => {
+    expect(circum(radius)).toEqual(2 * Math.PI * radius);
+  });
+});
