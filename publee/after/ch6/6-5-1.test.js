7b518d2165d797c9fe332e78e3530e520b3d9a12
publee <publizm@gmail.com>
2022-08-16 17:46:23 +0900

    [이철환] 6-5-1 리팩토링 반영 테스트코드 작성

--- publee/after/ch6/6-5-1.test.js
+++ publee/after/ch6/6-5-1.test.js
@@ -1,8 +1,8 @@
-import { circum } from "./6-5-1.js";
+import { getCircumferenceOfCircle } from "./6-5-1.js";
 
 describe("6-5-1 Test", () => {
   const radius = 3;
   it("radius 값의 반지름을 가진 원의 둘레는 2 * Math.PI * radius 이다.", () => {
-    expect(circum(radius)).toEqual(2 * Math.PI * radius);
+    expect(getCircumferenceOfCircle(radius)).toEqual(2 * Math.PI * radius);
   });
 });
