dcb9b1286cfda9fba3db6d740756e18ca01f9261
publee <publizm@gmail.com>
2022-08-16 18:10:37 +0900

    [이철환] 6-5-3 리팩토링 반영 테스트코드 작성

--- publee/after/ch6/6-5-3.test.js
+++ publee/after/ch6/6-5-3.test.js
@@ -1,4 +1,4 @@
-import { inNewEngland } from "./6-5-3.js";
+import { isCustomerAddressIncludedInNewEngland } from "./6-5-3.js";
 
 describe("6-5-3 Test", () => {
   let customer;
@@ -11,12 +11,12 @@ describe("6-5-3 Test", () => {
   });
 
   it("고객의 주소지가 new england에 포함될 경우 true를 반환한다.", () => {
-    expect(inNewEngland(customer)).toBe(true);
+    expect(isCustomerAddressIncludedInNewEngland(customer)).toBe(true);
   });
 
   it("고객의 주소지가 new england에 포함되지 않을 경우 false를 반환한다.", () => {
     expect(
-      inNewEngland({
+      isCustomerAddressIncludedInNewEngland({
         address: {
           state: "AB",
         },
