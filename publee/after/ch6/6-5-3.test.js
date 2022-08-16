9ab05b4e7f4d764ec0a0bb69cb1fefcd153c14d6
publee <publizm@gmail.com>
2022-08-16 18:06:09 +0900

    [이철환] 6-5-3 통과 테스트 코드 작성

---
+++ publee/after/ch6/6-5-3.test.js
@@ -0,0 +1,26 @@
+import { inNewEngland } from "./6-5-3.js";
+
+describe("6-5-3 Test", () => {
+  let customer;
+  beforeEach(() => {
+    customer = {
+      address: {
+        state: "CT",
+      },
+    };
+  });
+
+  it("고객의 주소지가 new england에 포함될 경우 true를 반환한다.", () => {
+    expect(inNewEngland(customer)).toBe(true);
+  });
+
+  it("고객의 주소지가 new england에 포함되지 않을 경우 false를 반환한다.", () => {
+    expect(
+      inNewEngland({
+        address: {
+          state: "AB",
+        },
+      })
+    ).toBe(false);
+  });
+});
