cfdaac45dc6943bbc0219b018f836a275dca9010
publee <publizm@gmail.com>
2022-08-16 17:03:56 +0900

    [이철환] 6-3-1 테스트코드 검증 기능 추가

--- publee/after/ch6/6-3-1.test.js
+++ publee/after/ch6/6-3-1.test.js
@@ -9,6 +9,24 @@ describe("6-3-1 Test", () => {
     };
   });
 
+  describe("Error Case - Invalid Value", () => {
+    it("When a null value is entered", () => {
+      expect(() => price(null)).toThrow("잘못된 값이 입력됬습니다");
+    });
+    it("When a Empty value is entered", () => {
+      expect(() => price()).toThrow("잘못된 값이 입력됬습니다");
+    });
+    it("When a Array is entered", () => {
+      expect(() => price([])).toThrow("잘못된 값이 입력됬습니다");
+    });
+    it("When a Number is entered", () => {
+      expect(() => price(0)).toThrow("잘못된 값이 입력됬습니다");
+    });
+    it("When a String is entered", () => {
+      expect(() => price("Hello")).toThrow("잘못된 값이 입력됬습니다");
+    });
+  });
+
   it("price function return valid value", () => {
     expect(price(order)).toBe(5100);
   });
