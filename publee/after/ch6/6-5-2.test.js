b5f225386adc828d80d823dece60f815aa25e9e1
publee <publizm@gmail.com>
2022-08-16 17:59:32 +0900

    [이철환] 6-5-2 리팩토링 반영 테스트코드 작성

--- publee/after/ch6/6-5-2.test.js
+++ publee/after/ch6/6-5-2.test.js
@@ -7,11 +7,11 @@ describe("6-5-2 Test", () => {
   });
 
   it("예약자를 추가 및 확인이 가능하다.", () => {
-    book.addReservation({ id: 1 });
-    expect(book.hasReservation({ id: 1 })).toBe(true);
+    book.addReservedCustomers({ id: 1 });
+    expect(book.isExistReservedCustomers({ id: 1 })).toBe(true);
   });
 
   it("예약 안 된 사람의 경우 false 값을 반환한다.", () => {
-    expect(book.hasReservation({ id: 1 })).toBe(false);
+    expect(book.isExistReservedCustomers({ id: 1 })).toBe(false);
   });
 });
