8a0deba7b195817735c492e8ae5474fbd1c6deb5
publee <publizm@gmail.com>
2022-08-24 06:44:44 +0900

    [이철환] 6-9 여러 함수를 클래스로 묶기 리팩토링 재완료

--- publee/after/ch6/6-9.test.js
+++ publee/after/ch6/6-9.test.js
@@ -4,6 +4,7 @@ describe("6-9 test", () => {
   it("year 2017 test", () => {
     const rowReading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
     const aReading = new Reading(rowReading);
+    expect(aReading.customer).toEqual("ivan");
     expect(aReading.baseRate).toEqual(0.1);
     expect(aReading.calculateBaseCharge).toEqual(1);
     expect(aReading.taxableCharge).toEqual(0.9);
@@ -13,7 +14,7 @@ describe("6-9 test", () => {
     const rowReading = { customer: "ivan", quantity: 10, month: 5, year: 2016 };
     const aReading = new Reading(rowReading);
     expect(aReading.baseRate).toEqual(0.2);
-    expect(aReading.calculateBaseCharge).toEqual(1);
-    expect(aReading.taxableCharge).toEqual(0.8);
+    expect(aReading.calculateBaseCharge).toEqual(2);
+    expect(aReading.taxableCharge).toEqual(1.9);
   });
 });
