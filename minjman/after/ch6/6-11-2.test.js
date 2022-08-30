338e8c8c05e80643799d29da5dc4ac311655eb6b
minjman2659 <minjman2659@gmail.com>
2022-08-31 00:15:29 +0900

    [김민재] 6-11-2 수정

--- minjman/after/ch6/6-11-2.test.js
+++ minjman/after/ch6/6-11-2.test.js
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import { jest } from '@jest/globals'
-import { getOrdersLength } from './6-11-2';
-import { getOrders } from './utils/6-11-2';
+import { run } from './6-11-2';
 
 describe('6-11-2 Test', () => {
     let argv = [];
@@ -11,21 +10,8 @@ describe('6-11-2 Test', () => {
     });
     describe('[Success]', () => {
         it('should get orders', () => {
-            const { orders } = getOrders(argv);
-            expect(orders).toEqual([
-                { id: 1, name: '우유', status: 'ready' },
-                { id: 2, name: '사과', status: 'not-ready' },
-                { id: 3, name: '바나나', status: 'ready' }
-            ]);
-        });
-        it('should get all orders length', () => {
-            const ordersLength = getOrdersLength(argv);
-            expect(ordersLength).toBe(3);
-        });
-        it('should get orders length only status is "ready"', () => {
-            argv[3] = '-r';
-            const ordersLength = getOrdersLength(argv);
-            expect(ordersLength).toBe(2);
+            const orderCount = run(argv);
+            expect(orderCount).toBe(3);
         });
     });
 });
